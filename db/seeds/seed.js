b3764ba52859cc262dd107c4bfb85e1665e61b6a
Ellis Lee <ellismckenzielee@gmail.com>
2021-11-01 10:44:45 +0000

    adds functionality to drop tables from db if they exist

--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -1,7 +1,21 @@
-const seed = (data) => {
-  const { articleData, commentData, topicData, userData } = data;
-  // 1. create tables
-  // 2. insert data
+const db = require("../connection");
+const format = require("pg-format");
+
+const seed = ({ articleData, commentData, topicData, userData }) => {
+  return db
+    .query("DROP TABLE IF EXISTS comments")
+    .then(() => {
+      return db.query("DROP TABLE IF EXISTS articles");
+    })
+    .then(() => {
+      const removeTopics = db.query("DROP TABLE IF EXISTS topics");
+      const removeUsers = db.query("DROP TABLE IF EXISTS users");
+      return Promise.all([removeTopics, removeUsers]);
+    })
+    .then(() => {
+      console.log("Tables successfully removed!");
+    })
+    .catch((err) => console.log(err));
 };
 
 module.exports = seed;

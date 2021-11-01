f52caec781e91383ea4c3845e36665d02d011354
Ellis Lee <ellismckenzielee@gmail.com>
2021-11-01 15:12:24 +0000

    sets up app.js test file

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,6 +1,13 @@
-const db = require('../db/connection.js');
-const testData = require('../db/data/test-data/index.js');
-const { seed } = require('../db/seeds/seed.js');
+const db = require("../db/connection.js");
+const testData = require("../db/data/test-data/index.js");
+const { seed } = require("../db/seeds/seed.js");
+const request = require("supertest");
 
 beforeEach(() => seed(testData));
 afterAll(() => db.end());
+
+describe("testing app.js", () => {
+  describe("/api/topics", () => {
+    it("status: 200. responds with all topics", () => {});
+  });
+});

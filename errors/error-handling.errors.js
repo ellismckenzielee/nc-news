6d25758828433b847bf367a55ecb9bcf1a5ca17e
Ellis Lee <ellismckenzielee@gmail.com>
2021-11-02 10:23:32 +0000

    adds console.log to error handling middleware

--- errors/error-handling.errors.js
+++ errors/error-handling.errors.js
@@ -1,10 +1,11 @@
 exports.handleCustomErrors = (err, req, res, next) => {
+  console.log("in handleCustomErrors error handler");
   if (err.status) res.status(err.status).send({ msg: err.msg });
   else next(err);
 };
 
 exports.handlePSQLErrors = (err, req, res, next) => {
-  console.log(err);
+  console.log("in handlePSQLErrors error handler");
   if (err.code) {
     console.log(err.code);
     const errorConverter = {
@@ -21,9 +22,11 @@ exports.handlePSQLErrors = (err, req, res, next) => {
 };
 
 exports.handle500 = (req, res, next) => {
+  console.log("in handle500 error handler");
   res.status(500).send({ msg: "server error" });
 };
 
 exports.handle404 = (req, res, next) => {
+  console.log("in handle404 error handler");
   res.status(404).send({ msg: "Invalid URL" });
 };

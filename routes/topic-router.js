5e5c7f7fb99e507870a0c410e29c59692a4ef603
Ellis Lee <ellismckenzielee@gmail.com>
2021-11-04 21:28:35 +0000

    adds POST /api/topics route

--- routes/topic-router.js
+++ routes/topic-router.js
@@ -1,6 +1,6 @@
 const topicRouter = require("express").Router();
-const { getTopics } = require("../controllers/topics.controllers");
+const { getTopics, postTopic } = require("../controllers/topics.controllers");
 const { invalidMethod } = require("../controllers/errors.controllers");
 
-topicRouter.route("/").get(getTopics).all(invalidMethod);
+topicRouter.route("/").get(getTopics).post(postTopic).all(invalidMethod);
 module.exports = topicRouter;

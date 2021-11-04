737eed028029c780a7ea56da530ba9c9827de705
Ellis Lee <ellismckenzielee@gmail.com>
2021-11-04 21:29:45 +0000

    adds POST functionality to /api/topics endpoint

--- controllers/topics.controllers.js
+++ controllers/topics.controllers.js
@@ -1,8 +1,16 @@
-const { selectTopics } = require("../models/topics.models");
+const { selectTopics, insertTopic } = require("../models/topics.models");
 exports.getTopics = (req, res, next) => {
   selectTopics()
     .then((topics) => {
       res.status(200).send({ topics });
     })
     .catch(next);
+};
+
+exports.postTopic = (req, res, next) => {
+  console.log("in postTopic controller function");
+  const { slug, description } = req.body;
+  insertTopic(slug, description).then((topic) => {
+    res.status(201).send({ topic });
+  });
 };
--- models/topics.models.js
+++ models/topics.models.js
@@ -5,3 +5,16 @@ exports.selectTopics = () => {
     return rows;
   });
 };
+
+exports.insertTopic = (slug, description) => {
+  console.log("in insertTopic model");
+  console.log(slug, description);
+  return db
+    .query(
+      "INSERT INTO topics (slug, description) VALUES ($1, $2) RETURNING *;",
+      [slug, description]
+    )
+    .then(({ rows }) => {
+      return rows[0];
+    });
+};

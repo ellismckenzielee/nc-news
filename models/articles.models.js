7acedef275bfb0cb45b7c72700ab3c83bd2178f1
Ellis Lee <ellismckenzielee@gmail.com>
2021-11-04 22:25:52 +0000

    adds functionality to determine whether a 404 or 200 should be returned on GET /api/articles/article_id/comments endpoint

--- models/articles.models.js
+++ models/articles.models.js
@@ -82,6 +82,14 @@ exports.selectArticleComments = (article_id) => {
     )
     .then(({ rows }) => {
       if (rows.length > 0) return rows;
+      else {
+        return db.query("SELECT * FROM articles WHERE article_id = $1;", [
+          article_id,
+        ]);
+      }
+    })
+    .then(({ rows }) => {
+      if (rows.length) return [];
       return Promise.reject({ status: 404, msg: "article not found" });
     });
 };
